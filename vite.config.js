e5defcb46d69c79fa19cfc6cfda79ee275b2be8e
mounir-1999 <119586848+mounir-1999@users.noreply.github.com>
2026-02-26 09:24:40 +0200

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     // viteSingleFile(),
     vue(),
   ],
-   base: '/monir-character-test/',   // 🔥 MUST match repo name exactly
+   base: '/character-test/',   // 🔥 MUST match repo name exactly
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
